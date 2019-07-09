59926534edeced1cc7a19f66ae3f2d751757dae3
Kent G. Budge <kgb@kgbudge.com>
2019-07-08 22:22:56 -0600

    update broken out as separate function. Prepare to modify Gf calculation to use vector of Gfs.

--- src/main.cc
+++ src/main.cc
@@ -135,6 +135,16 @@ double Cl;
 string file;
 string name;
 
+void update(double const T, 
+            double const P, 
+            vector<Phase> phase,
+            bool const oxygen_specified, 
+            bool const oxygen_FMQ,
+            double &pO2,
+	        double Np[E_END],
+	        unsigned p[E_END],
+            double volume[E_END]);
+
 //-----------------------------------------------------------------------------//
 void update()
 {
@@ -266,11 +276,13 @@ void update()
 	// Initial composition
 	double Np[E_END];
 	unsigned p[E_END];
+	double volume[E_END];
 
 	double Gf[P_END];
 	initialize_Gf(T, P, Gf);
 
 	bool oxygen_specified = !button_oxygen_by_composition->get_active();
+	bool oxygen_FMQ;
 	double pO2, GfO2;
 	if (oxygen_specified)
 	{
@@ -279,12 +291,14 @@ void update()
 			text = entry_pO2->get_text();
 			pO2 = atof(text.c_str());
 			GfO2 = phase[P_O2].model->Gf(phase[P_O2], T, pO2);
+			oxygen_FMQ = false;
 		}
 		else
 		{
 			Check(button_oxygen_FMQ->get_active());
 			GfO2 = 2*Gf[P_MAGNETITE]+3*Gf[P_SiO2_QUARTZ]-3*Gf[P_FAYALITE];
 			pO2 = phase[P_O2].model->P(phase[P_O2], T, GfO2);
+			oxygen_FMQ = true;
 		}
 	}
 
@@ -348,205 +362,15 @@ void update()
 	p[E_ZR] = P_ZrO2;
 	Np[E_ZR] = nZrO2;
 	
-	// Allocate storage for linear algebra operations
-
-	unsigned const N = E_END;
-	gsl_matrix *A = gsl_matrix_alloc(N, N);
-	gsl_matrix *V = gsl_matrix_alloc(N, N);
-	gsl_vector *S = gsl_vector_alloc(N);
-	gsl_vector *work = gsl_vector_alloc(N);
-	gsl_vector *b = gsl_vector_alloc(N);
-	gsl_vector *x = gsl_vector_alloc(N);	
-	gsl_vector *aGf = gsl_vector_alloc(P_END);
-	gsl_permutation *permute = gsl_permutation_alloc(P_END);
-	double Ainv[N][N];
-	double left[N];
-	
-	// now iterate to find composition
-
-	double element_activity[E_END];
-	for(;;)
-	{
-		// find absolute free energy of elements
-
-		// Build a linear system and solve for element free energies
-		
-		gsl_matrix_set_zero(A);
-		gsl_vector_set_zero(b);
-
-		if (oxygen_specified)
-		{
-			Gf[P_O2] = GfO2;
-		}
-
-		for (unsigned i=0; i<N; ++i)
-		{
-			unsigned const pi = p[i];
-			gsl_vector_set(b, i, -Gf[pi]);
-			unsigned const nz = phase[pi].nz;
-			for (unsigned j=0; j<nz; j++)
-			{
-				gsl_matrix_set(A, i, phase[pi].z[j], phase[pi].n[j]);
-			}
-		}
-
-		gsl_linalg_SV_decomp (A, V, S, work);		
-		gsl_linalg_SV_solve (A, V, S, b, x);
-
-		// Compute absolute free energies of phases
-
-		for (unsigned i=0; i<E_END; ++i)
-		{
-			element_activity[i] = gsl_vector_get(x, i);
-		}
-
-		for (unsigned i=0; i<P_END; ++i)
-		{
-			double aGfi = Gf[i];
-			for (unsigned j=0; j<phase[i].nz; j++)
-			{
-				aGfi += phase[i].n[j]*element_activity[phase[i].z[j]];
-			}
-			gsl_vector_set(aGf, i, aGfi);
-		}
-	
-		gsl_sort_vector_index(permute, aGf);
-
-		// Find a composition basis for the current composition.
-
-		for (unsigned i=0; i<N; i++)
-		{
-			for (unsigned j=0; j<N; j++)
-			{
-				double sum = 0.0;
-				for (unsigned k=0; k<N; ++k)
-				{
-					double sk = gsl_vector_get(S, k);
-					if (fabs(sk)>1.0e-10)
-					{
-						sum += gsl_matrix_get(V, i, k)*gsl_matrix_get(A, j, k)/sk;
-					}
-				}
-				Ainv[i][j] = sum;
-			}
-		}
-		
-		for (unsigned ii=0; ii<P_END; ++ii)
-		{
-			unsigned const ip = gsl_permutation_get(permute, ii);
-			if (oxygen_specified && ip == P_O2)
-				continue;
-
-			double aGfi = gsl_vector_get(aGf, ip); 
-			if (fabs(aGfi)<1.0e-9)
-			{
-				goto DONE;
-			}
-
-			// Construct an equation that reduces the Gibbs function further.
-
-			// Construct the reaction
-
-			fill(left, left+N, 0.0);
-			for (unsigned i=0; i<phase[ip].nz; ++i)
-			{
-				unsigned z = phase[ip].z[i];
-				unsigned n = phase[ip].n[i];
-				for (unsigned j=0; j<N; ++j)
-				{
-					left[j] += n*Ainv[z][j];
-				}
-			}
-
-			// Can the reaction proceed?
-
-			double r = numeric_limits<double>::max();
-			double rGf = Gf[ip];
-			for (unsigned i=0; i<N; ++i)
-			{
-				if (left[i]>1.0e-10)
-				{
-					r = min(r, Np[i]/left[i]);
-					if (r<=0.0)
-					{
-						break;
-					}
-				}
-				rGf -= left[i]*Gf[p[i]];
-			}
-
-			if (r<=1.0e-10)
-			{
-				continue;
-			}
-
-			// Yes, the reaction has somewhere to go
-
-			cout << "Performing reaction ";
-			bool first = true;
-			for (unsigned i=0; i<N; ++i)
-			{
-				if (left[i]>1.0e-10)
-				{
-					if (!first) 
-					{
-						cout << " + ";
-					}
-					first = false;
-					if (fabs(left[i]-1.0)>1.0e-10)
-					{
-						cout << setprecision(4) << left[i];
-					}
-					cout << phase[p[i]].name;
-				}
-			}
-			cout << " -> " << phase[ip].name;
-			
-			for (unsigned i=0; i<N; ++i)
-			{
-				if (left[i]<-1.0e-10)
-				{
-					cout << " + ";
-					first = false;
-					if (fabs(left[i]+1.0)>1.0e-10)
-					{
-						cout << setprecision(4) << -left[i];
-					}
-					cout << phase[p[i]].name;
-				}
-			}
-			cout << endl;
-
-			unsigned n;
-			for (unsigned i=0; i<N; ++i)
-			{
-				Np[i] -= r*left[i];
-				if (fabs(Np[i])<1.0e-10)
-				{
-					if (fabs(left[i])>1.0e-10)
-					{
-						n = i;
-					}
-					Np[i] = 0.0;
-				}
-			}
-			Np[n] = r;	
-			p[n] = ip;
-
-			break;
-		}
-	}
-
-
-	DONE:
-	gsl_matrix_free(A);
-	gsl_matrix_free(V);
-	gsl_vector_free(S);
-	gsl_vector_free(work);
-	gsl_vector_free(b);
-	gsl_vector_free(x);
-	gsl_vector_free(aGf);
-	gsl_permutation_free(permute);
+    update(T, 
+           P, 
+           vector<Phase>(phase, phase+P_END),
+           oxygen_specified, 
+           oxygen_FMQ,
+           pO2,
+	       Np,
+	       p,
+           volume);
 
 	CIPW_Columns m_Columns;
 	Glib::RefPtr<Gtk::ListStore> list_store_CIPW = Gtk::ListStore::create(m_Columns);
@@ -555,42 +379,20 @@ void update()
 	Gtk::TreeModel::iterator iter;
 	Gtk::TreeModel::Row row;
 
-	if (true || !oxygen_specified)
-	{
-		pO2 = phase[P_O2].model->P(phase[P_O2], T, -2*element_activity[E_O]);
-		entry_pO2->set_text(tostring(pO2));
-	}
+	entry_pO2->set_text(tostring(pO2));
 		
 	// Convert to volume fraction
 
-	double volume[N];
-
 	double Vtot = 0.0;
-
-	for (unsigned i=0; i<N; ++i)
+	for (unsigned i=0; i<E_END; ++i)
 	{
 		unsigned const pi = p[i];
         volume[i] = Np[i]*phase[pi].model->volume(phase[pi], T, P);
 		Vtot += volume[i];
-		/*
-		switch (phase[pi].model)
-		{
-			case SOLID:
-			case MELT:  
-				volume[i] = Np[i]*phase[pi].V;
-				Vtot += volume[i];
-				break;
-
-			case VAPOR:
-				volume[i] = Np[i]*R*T/solve_for_fugacity(phase[pi], T, Gf[pi]);
-				Vtot += volume[i];
-				break;
-		}
-		*/
 	}
 		
 	rnorm = 100.0/Vtot;
-	for (unsigned i=0; i<N; ++i)
+	for (unsigned i=0; i<E_END; ++i)
 	{
 		volume[i] *= rnorm;
 		if (volume[i]>0.0)
